0072bb65cc4b73492c23bc20765c125afb79a5ee
John Carlo Malto <jakemalto2014@gmail.com>
2016-06-26 23:05:43 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,23 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
-	
+	it('should filter on food correctly', function () {
+		 var mockedList = [{
+				 name: 'Test',
+					 favoriteFood: 'Pittsburgh'
+		 }, {
+				 name: 'Test 2',
+					 favoriteFood: 'New York'
+		 }, {
+				 name: 'Test 3',
+					 favoriteFood: 'Chicago'
+		 }];
+
+			var results = $filter('favoriteFood')(mockedList, 'Chicago');
+
+			expect(results.length).toBe(1);
+			expect(results[0].name).toBe('Test 3');
+	});
+
+
 });
